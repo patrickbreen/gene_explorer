12d9bf92e09cca8e7d4cf9e25ffdf5302811d3b8
tecleatu <pbreen.uga.edu>
2015-01-05 13:46:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeneExplorer::Application.config.secret_key_base = 'fa4cfd2064569d37e55486c7db9bc082ed2af47f985ca3e7fc63d0f3a30ccc6f9483c0700babfd43042806214a3e4b342279341bd22df074c8e2806f937ce339'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GeneExplorer::Application.config.session_store :cookie_store, key: '_gene_explorer_session'
